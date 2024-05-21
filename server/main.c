239c1b8bb7abf391a5be6535b7ab369c9662cea5
Tom Blancheton <tom.blancheton@epitech.eu>
2024-05-21 09:50:21 +0200

    [FIX] server files move in C

---
+++ server/main.c
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2024
+** B-YEP-400-LYN-4-1-zappy-tom.blancheton
+** File description:
+** main.c
+*/
+
+#include "zappy_server.hpp"
+#include "stdio.h"
+
+int main(int ac, char **av)
+{
+    (void) ac;
+    (void) av;
+
+    printf("Salam server !\n");
+    return 0;
+}
--- server/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "zappy_server.hpp"
-
-int main(int ac, char **av)
-{
-    return 0;
-}
