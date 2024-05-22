05b2becdfcd051f7f75522ce111423f0203aca39
avalancheeee <cyprien.diederichs@epitech.eu>
2024-05-22 15:43:40 +0200

    docs(ai): new connexion docstring

--- ai/src/server/connexion.py
+++ ai/src/server/connexion.py
@@ -1,8 +1,15 @@
 import socket
-import re
 
 
-def connect(port, team_name, host) -> list[int]:
+def connect(port: str, team_name: str, host: str) -> tuple[list[int], socket]:
+    """
+    The connect function establishes a TCP connection to a specified host and port, sends a team name, and processes the received data into a list of integers, returning this list along with the socket object.
+
+    :param port: The port number to connect to.
+    :param team_name: A string representing the name of the team.
+    :param host: The hostname or IP address to connect to.
+    :return: A tuple containing a list of integers and the socket object used for the connection.
+    """
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((host, int(port)))
     welcome_message = client_socket.recv(1024).decode().strip()
@@ -10,8 +17,8 @@ def connect(port, team_name, host) -> list[int]:
     client_socket.send(f"{team_name}\n".encode())
     cli_number = client_socket.recv(1024).decode()
     print(f"Received: {cli_number}")
-    new = re.split(' |\\n', cli_number)
+    new = cli_number.split()
     print(new)
-    result = [int(x) for x in new if x != '']
+    result = list(map(int, filter(None, new)))
     print(result)
-    return result
+    return result, client_socket
