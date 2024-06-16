6ee3e1412ac346778ded20b03ffe1bd011fc520e
Tom Blancheton <tom.blancheton@epitech.eu>
2024-06-17 01:54:40 +0200

    feat: pdr and pgt to GUI

--- server/src/commands/player/command_player2.c
+++ server/src/commands/player/command_player2.c
@@ -31,7 +31,7 @@ void cmd_take(server_t *server, char *args, client_socket_t *client)
 {
     player_t *player = client->player;
     char **parse_args = decompose_output(args);
-    int object_taken = atoi(parse_args[1]);
+    int obj_taken = atoi(parse_args[1]);
     int result;
 
     if (!player) {
@@ -39,12 +39,13 @@ void cmd_take(server_t *server, char *args, client_socket_t *client)
         return;
     }
     result = player_take_item(player,
-    server->grid->_tiles[player->_pos._y][player->_pos._y], object_taken);
+    server->grid->_tiles[player->_pos._y][player->_pos._y], obj_taken);
     if (result == 0)
         dprintf(client->socket, "ok\n");
     else
         dprintf(client->socket, "ko\n");
     free_str_array(parse_args);
+    dprintf(get_gui(server)->socket, "pgt #%d %d\n", client->_id, obj_taken);
     printf("take\n");
 }
 
@@ -52,7 +53,7 @@ void cmd_set(server_t *server, char *args, client_socket_t *client)
 {
     player_t *player = client->player;
     char **parse_args = decompose_output(args);
-    int object_set = atoi(parse_args[1]);
+    int obj_set = atoi(parse_args[1]);
     int result;
 
     if (!player) {
@@ -60,11 +61,12 @@ void cmd_set(server_t *server, char *args, client_socket_t *client)
         return;
     }
     result = player_take_item(player,
-    server->grid->_tiles[player->_pos._y][player->_pos._y], object_set);
+    server->grid->_tiles[player->_pos._y][player->_pos._y], obj_set);
     if (result == 0)
         dprintf(client->socket, "ok\n");
     else
         dprintf(client->socket, "ko\n");
     free_str_array(parse_args);
+    dprintf(get_gui(server)->socket, "pdr #%d %d\n", client->_id, obj_set);
     printf("set\n");
 }
