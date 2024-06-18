4a22fdec95518d9f0650a52a2c326f5f215596b7
Tom Blancheton <tom.blancheton@epitech.eu>
2024-06-18 10:30:29 +0200

    feat: add PDI to GUI

--- server/src/player/player_dead.c
+++ server/src/player/player_dead.c
@@ -19,6 +19,7 @@ void client_dead(server_t *server, client_socket_t *client)
         }
     }
     dprintf(client->socket, "dead\n");
+    dprintf(get_gui(server)->socket, "pdi %d\n", client->_id);
     close(client->socket);
     TAILQ_REMOVE(&server->_head_client_sockets, client, entries);
     free_player(client->player);
